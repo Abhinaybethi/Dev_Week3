d5d648721a9c0ad4cc8c3e4e63315f107dc5dc46
Abhinaybethi <abhinaybethi26@gmail.com>
2024-11-22 22:26:49 +0530

    initial commit of prima

---
+++ Prime.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(number + (isPrime ? " is a prime number." : " is not a prime number."));
+    }
+}
